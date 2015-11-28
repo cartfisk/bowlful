eb4cd5aea11b344c767b4e8bafcd520593a0b2e9
Carl Reider <creider@luc.edu>
2015-11-28 14:36:55 -0600

    Adding Plugin Support

--- bowlful-2/platforms/android/assets/www/cordova_plugins.js
+++ bowlful-2/platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,35 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.1",
+    "cordova-plugin-device": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
