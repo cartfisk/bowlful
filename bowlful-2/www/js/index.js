2bb3411921c04629f049b4818b17a589637dace6
Carl Reider <creider@luc.edu>
2015-11-28 19:33:08 -0600

    Got the deviceready EVent to fire. Trying to add support for the plugins now

--- bowlful-2/www/js/index.js
+++ bowlful-2/www/js/index.js
@@ -1,25 +1,9 @@
-var app = {
-  initialize: function(){
-    this.bindEvents();
-  }
+$(document).on("pageinit","#login", function(){
+  document.addEventListener('deviceready',onDeviceReady,false);
+});
 
-
-  bindEvents:function(){
-    document.addEventListener('deviceready',this.onDeviceReady,false);
-  },
-
-  onDeviceReady: function(){
-    app.recievedEvent('deviceready');
-  },
-
-  recievedEvent:function(id){
-    var parentElement = document.getElementsById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var recievedElement = parentElement.querySelector('.recieved')
-    listeningElement.setAttribute('style','display:none;');
-    recievedElement.setAttribute('style','display:block;');
-
-    console.log('Recieved Event: ' + id);
-  }
-};
-app.initialize();
+function onDeviceReady(){
+  //code goes here
+  console.log('success - js call');
+  alert(" Events have been bound");
+}
